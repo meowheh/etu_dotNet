10a0fb89462e9181ee1fdeaff01e80d7435235e4
meowheh <theshapeand@gmail.com>
2021-03-21 14:50:12 +0300

    Добавление связного списка

---
+++ Lab1/List.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    //класс элемента списка
+    public class ListNode<T>
+    {
+        public ListNode(T data)
+        {
+            Data = data;
+        }
+        public T      Data { get; set; }
+        public ListNode<T> Next { get; set; }
+    }
+    //класс связного списка
+    //наследуемся от интерфейса перечисления
+    public class List<T> : IEnumerable<T>
+    {
+        private ListNode<T> _first;       //первый элемент
+        private ListNode<T> _last;        //последний элемент
+        private int         count;        //количество элементов
+        
+        //добавление элемента в конец списка
+        public void Add(T data)
+        {
+            var node = new ListNode<T>(data);
+            if (_first != null)
+            {
+                _last.Next = node;
+            }
+            else
+            {
+                _first = node;
+            }
+            _last = node;
+            count++;
+        }
+        //добавление элемента в начало списка
+        public void AddFirst(T data)
+        {
+            ListNode<T> node = new ListNode<T>(data) {Next = _first};
+            _first = node;
+            if (count == 0)
+                _last = _first;
+            count++;
+        }
+        //добавление элемента в произвольную позицию 
+        public void insert(int pos, T data)
+        {
+            if(pos == 0)
+                AddFirst(data);
+            else if(pos == count)
+                Add(data);
+            else if (pos > 0 && pos < count)
+            {
+                var previous = _first;
+                for (var i = 0; i < pos - 1; i++)
+                    previous = previous.Next;
+                var next = previous.Next;
+                var node = new ListNode<T>(data) {Next = next};
+                previous.Next = node;
+                count++;
+            }
+        }
+        //удалить первый элемент
+        public bool RemoveFirst()
+        {
+            if (_first != null)
+            {
+                _first = _first.Next;
+                if (_first == null)
+                    _last = null;
+                count--;
+                return true;
+            }
+            return false;
+        }
+        //удалить последний элемент
+        public bool RemoveLast()
+        {
+            var previous = _first;
+            if (previous != null)
+            {
+                while (previous.Next != _last)
+                {
+                    previous = previous.Next;
+                }
+                previous.Next = null;
+                _last = previous;
+                count--;
+                return true;
+            }
+            return false;
+        }
+        //удаление элемента из списка по индексу
+        public bool Remove(int index)
+        {
+            if (index >= count || index < 0)
+                return false;
+            if (index == count - 1)
+                RemoveLast();
+            else if (index == 0)
+                RemoveFirst();
+            else
+            {
+                var previous = _first;
+                for (var i = 0; i < index - 1; i++)
+                {
+                    previous = previous.Next;
+                }
+                var current = previous.Next;
+                var next = current.Next;
+                previous.Next = next;
+                current.Next = null;
+                count--;
+                return true;
+            }
+            return false;
+        }
+        public void Reverse()
+        {
+            ListNode<T> current  = _first,
+                        previous = null;
+            while (current != null)
+            {
+                var next = current.Next;
+                next = current.Next;
+                current.Next = previous;
+                previous = current;
+                current = next;
+            }
+            _last = _first;
+            _first = previous;
+            
+
+        }
+        // реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+ 
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            var current = _first;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+        
+        
+        
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static int N = 10;
+        public static void listOutput(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void testList()
+        {
+            List<int> m_list = new List<int>();
+            for(int i = 0; i < N; i++)
+                m_list.Add(i);
+            Console.Write("output: "); listOutput(m_list);
+            int pos = 7, data = -5;
+            m_list.insert(pos,data);
+            Console.Write("insert " + data +" in pos "+ pos +": "); listOutput(m_list);
+            pos = 3;
+            m_list.Remove(pos);
+            Console.Write("remove from pos " + pos +": "); listOutput(m_list);
+            
+            m_list.RemoveFirst();
+            Console.Write("remove the first element: "); listOutput(m_list);
+            
+            m_list.RemoveLast();
+            Console.Write("remove the last element: "); listOutput(m_list);
+            data = -10;
+            m_list.AddFirst(data);
+            Console.Write("add the fisrt element " + data +": "); listOutput(m_list);
+            
+            m_list.Reverse();
+            Console.Write("reverse list: "); listOutput(m_list);
+            
+            m_list.Reverse();
+            Console.Write("reverse list: ");listOutput(m_list);
+        }
+        public static void Main(string[] args)
+        {
+            testList();
+        }
+    }
+}
